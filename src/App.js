697915cf59d28b54578f9036e4a7e92470c030cb
sukanya <sukanyamuthkuru46@gmail.com>
2024-04-13 18:10:20 +0000

    heading and para

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css'
+
+const cardsList = [
+  {
+    id: 1,
+    title: 'Data Scientist',
+    description:
+      'Data scientists gather and analyze large sets of structured and unstructured data',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/data-scientist-img.png',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    title: 'IOT Developer',
+    description:
+      'IoT Developers are professionals who can develop, manage, and monitor IoT devices.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/iot-developer-img.png',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    title: 'VR Developer',
+    description:
+      'A VR developer creates completely new digital environments that people can see.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/vr-developer-img.png',
+    className: 'card-3',
+  },
+  {
+    id: 4,
+    title: 'ML Engineer',
+    description:
+      'Machine learning engineers feed data into models defined by data scientists.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/ml-engineer-img.png',
+    className: 'card-4',
+  },
+]
+
+const App = () => (
+  <div className="bg-container">
+    <h1 className="heading">Learn 4.0 Technologies</h1>
+    <p
+      className="Get trained by alumni of IITs and top companies like Amazon,Microsoft,intel
+        nvidia,quolcum e.t.c.Learn diretly from professionals involed in Product Development"
+    ></p>
+  </div>
+)
+
+export default App
